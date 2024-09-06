d833014ddc1eb1ac9fb33e5193565be51495aab9
magno <magnobatista75@gmail.com>
2024-09-06 00:56:53 -0300

    atualizacao na saida de dados

--- src/matriz.js
+++ src/matriz.js
@@ -1,14 +1,25 @@
 //matriz
 //nome/studio/xp
 
+/* Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
+Se XP for menor do que 1.000 = Ferro
+Se XP for entre 1.001 e 2.000 = Bronze
+Se XP for entre 2.001 e 5.000 = Prata
+Se XP for entre 5.001 e 7.000 = Ouro
+Se XP for entre 7.001 e 8.000 = Platina
+Se XP for entre 8.001 e 9.000 = Ascendente
+Se XP for entre 9.001 e 10.000= Imortal
+Se XP for maior ou igual a 10.001 = Radiante
+*/
+
 let nomeXp = ""
 
 let nomesHerois = [
-    ["Homem Aranha", "Marvel", 5500],
-    ["Homem de Ferro", "Marvel", 7500],
-    ["Aquaman", "Dc", 8500],
-    ["Mulher Maravilha", "Dc", 9500],
-    ["Saitama", "Madhouse", 100000]
+    ["HOMEM ARANHA", "MARVEL", 5500],
+    ["HOME DE FERRO", "MARVEL", 7500],
+    ["AQUAMAN", "DC", 8500],
+    ["MULHER MARAVILHA", "DC", 9500],
+    ["SAITAMA", "MADHOUSE", 100000]
 ]
 
 for (i = 0; i < nomesHerois.length; i++) {
@@ -37,5 +48,6 @@ for (i = 0; i < nomesHerois.length; i++) {
         nomeXp = "Radiante";
     }
 
-    console.log("O Heroi " + nomesHerois[i][0] + " De Xp : " + nomesHerois[i][2] + " Está no nível de : " + nomeXp)
+    console.log("O Heroi " + nomesHerois[i][0] + " De Xp : " + nomesHerois[i][2] + " Está no nível de : " + nomeXp.toUpperCase() + "\n" + nomesHerois[i][1] )
 }
+
